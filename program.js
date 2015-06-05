19cd924622bd6fdf71efd73aa8693379259d5d26
colinhoulihan <colinhoulihan@gmail.com>
2015-06-05 23:02:43 +0100

    adding My Calculator

---
+++ program.js
@@ -0,0 +1,12 @@
+/* console.log(process.argv[2])
+console.log(process.argv[3])
+console.log(process.argv[4])
+*/
+
+var firstNum = Number(process.argv[2])
+var secondNum = Number(process.argv[3])
+var thirdNum = Number(process.argv[4])
+
+
+var total = firstNum + secondNum + thirdNum
+console.log('the sum of the numbers is ' + total)
