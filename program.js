81b15e1801fb8a30ec37e19e9f2bb6e3a9fabb9c
colinhoulihan <colinhoulihan@gmail.com>
2015-06-05 23:09:53 +0100

    updating to add multiple operations

--- program.js
+++ program.js
@@ -3,10 +3,20 @@ console.log(process.argv[3])
 console.log(process.argv[4])
 */
 
-var firstNum = Number(process.argv[2])
-var secondNum = Number(process.argv[3])
-var thirdNum = Number(process.argv[4])
+var operation = Number(process.argv[2])
+var firstNum = Number(process.argv[3])
+var secondNum = Number(process.argv[4])
+var thirdNum = Number(process.argv[5])
+var total
 
+console.log(Number(process.argv[2]))
 
-var total = firstNum + secondNum + thirdNum
-console.log('the sum of the numbers is ' + total)
+if(operation==1)
+{
+  total = firstNum + secondNum + thirdNum
+  console.log('the sum of the numbers is ' + total)
+}
+else if (operation==2) {
+  total = firstNum * secondNum * thirdNum
+  console.log('the product of the numbers is '+ total)
+}
